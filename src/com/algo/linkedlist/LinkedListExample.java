69c411c4d84620f2b92d691655ff20da58834868
aravind87 <mailaravind08@gmail.com>
2016-06-06 20:57:29 -0700

    Initial version

--- src/com/algo/linkedlist/LinkedListExample.java
+++ src/com/algo/linkedlist/LinkedListExample.java
@@ -4,7 +4,7 @@ package com.algo.linkedlist;
  * Created by Aravind on 6/6/2016.
  */
 public class LinkedListExample {
-
+    Node start;
     class Node<T> {
         T data;
         Node<T> next;
